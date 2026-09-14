dc0fb0cb5b8bc2b67116eaa1b730387af6269778
DG2007LK <gimahandasun@gmail.com>
2026-09-14 20:26:10 +0530

    Update app.py

--- app.py
+++ app.py
@@ -19,7 +19,14 @@ def download():
         'format': 'best',
         'quiet': True,
         'no_warnings': True,
-        'extractor_args': {'youtube': {'player_client': ['android', 'web']}},
+        'extractor_args': {
+            'youtube': {
+                'player_client': ['ios', 'web']
+            }
+        },
+        'http_headers': {
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36'
+        }
     }
 
     try:
